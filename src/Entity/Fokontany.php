7e5328f81517996160f8b771472c7baf0491e030
julkwel <jrajerison@bocasay.com>
2024-01-13 20:15:16 +0300

    WIP ... custom

--- src/Entity/Fokontany.php
+++ src/Entity/Fokontany.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
@@ -22,6 +23,7 @@ use Symfony\Component\Uid\Uuid;
     normalizationContext: ['groups' => ['fokontany:read']]
 )]
 #[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'name' => 'partial'])]
+#[ApiFilter(OrderFilter::class, properties: ['name'], arguments: ['orderParameterName' => 'order'])]
 class Fokontany
 {
     #[ORM\Id]
--- src/Entity/Region.php
+++ src/Entity/Region.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
@@ -24,6 +25,7 @@ use Symfony\Component\Uid\Uuid;
     normalizationContext: ['groups' => ['region:read', 'commune:read', 'district:read', 'fokontany:read']]
 )]
 #[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'name' => 'partial'])]
+#[ApiFilter(OrderFilter::class, properties: ['name'], arguments: ['orderParameterName' => 'order'])]
 class Region
 {
     #[ORM\Id]
