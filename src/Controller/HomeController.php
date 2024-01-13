e40eb80cef7a3806a6c000827893cfa7d91b30c8
julkwel <jrajerison@bocasay.com>
2024-01-13 19:28:01 +0300

    WIP ( clean with soklin )

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,20 +8,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Class HomeController
+ *
+ * Default controller, will handle all feature request from end user
+ */
 class HomeController extends AbstractController
 {
     #[Route('/', 'home_page', methods: 'GET')]
-    public function homePage(Request $request): Response
+    public function homePage(): Response
     {
         return $this->render('homepage/index.html.twig');
-    }
-
-    public function handleFeatureRequest()
-    {
-
     }
 }
